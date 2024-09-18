73b72cb04e7b64b8399d4f29a0055e35d3ef15e9
raziqpv333 <raziqpv333@gmail.com>
2024-09-18 09:07:00 +0530

    staticadds

--- appflask.py
+++ appflask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 
 @app.route('/')
@@ -12,4 +12,24 @@ def fun2(a):
 @app.route('/data/<int:b>/<int:c>/<int:d>')    
 def fun3(b,c,d):
     return 'print laargest number' >b>c>d
+
+
+@app.route('/h1')
+def fun4():
+    a='welcome'
+    return render_template("index.html",a=a)
+@app.route('/first')
+def fun5():
+    return render_template("first.html")
+
+
+@app.route('/second')
+def fun6():
+    return render_template("second.html")
+
+
+
+@app.route('/third')
+def fun7():
+    return render_template("third.html")
 app.run()
